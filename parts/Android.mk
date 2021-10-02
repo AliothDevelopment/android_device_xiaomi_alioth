3504ba793ed046eb1219d99a26c974e400ce14d4
MB <mb150996@gmail.com>
2021-10-02 12:07:04 +0200

    Revert "sm8250-common: Add touch profiles for gaming and benchmarking"
    This reverts commit 073a5e4319e14d3d52c86cdd6bc39add49d9aaba.

--- parts/Android.mk
+++ parts/Android.mk
@@ -27,7 +27,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     vendor.xiaomi.hardware.displayfeature-V1.0-java \
-    vendor.xiaomi.hardware.touchfeature-V1.0-java
+    vendor.xiaomi.hardware.touchfeature-V1.0-java 
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
